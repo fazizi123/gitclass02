a51123468057222148fa3c374ad1051b49f3d408
fazizi123 <fazizi23@gmail.com>
2024-10-29 13:09:52 -0400

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,13 +1,10 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("this is the project version 2");
+
+        System.out.println("this is the project version 3");
 
 
     }
 }
-//done with version two
+//done with version three
